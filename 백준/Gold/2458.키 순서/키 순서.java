cd98591c2acda5d29380b2fa9925b08e6fbc0b4f
Yongjun Kwon <76172759+ubermen5che@users.noreply.github.com>
2022-02-18 14:34:45 +0900

    [Gold IV] Title: 키 순서, Time: 584 ms, Memory: 37664 KB -BaekjoonHub

--- 백준/Gold/2458.키 순서/키 순서.java
+++ 백준/Gold/2458.키 순서/키 순서.java
@@ -6,8 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M;
-    static int[][] map;
-
+    static int[][] graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,41 +14,40 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new int[N + 1][N + 1];
+        graph = new int[N + 1][N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            map[from][to] = 1;
+            graph[a][b] = 1;
         }
 
         for (int k = 1; k <= N; k++) {
             for (int i = 1; i <= N; i++) {
                 for (int j = 1; j <= N; j++) {
-                    if (i != j && map[i][k] != 0 && map[k][j] != 0) {
-                        if (map[i][j] == 0 || map[i][j] > map[i][k] + map[j][j]) {
-                            map[i][j] = map[i][k] + map[k][j];
+                    if (i != j && graph[i][k] != 0 && graph[k][j] != 0) {
+                        if (graph[i][j] == 0 || graph[i][j] > graph[i][k] + graph[k][j]) {
+                            graph[i][j] = graph[i][k] + graph[k][j];
                         }
                     }
                 }
             }
         }
 
+        int cnt;
         int answer = 0;
         for (int i = 1; i <= N; i++) {
-            int cnt = 0;
+            cnt = 0;
             for (int j = 1; j <= N; j++) {
-                if (map[i][j] != 0 || map[j][i] != 0) {
+                if (graph[i][j] != 0 || graph[j][i] != 0) {
                     cnt++;
                 }
             }
-
             if (cnt == N - 1)
                 answer++;
         }
-
         System.out.println(answer);
     }
 }
