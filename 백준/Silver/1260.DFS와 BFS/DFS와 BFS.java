f5f484ac5fa6e36ef475e3629e05083115be32c9
Yongjun Kwon <76172759+ubermen5che@users.noreply.github.com>
2022-02-22 16:33:28 +0900

    [Silver II] Title: DFS와 BFS, Time: 284 ms, Memory: 20948 KB -BaekjoonHub

--- 백준/Silver/1260.DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260.DFS와 BFS/DFS와 BFS.java
@@ -1,92 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
+    static int N, M, start;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int startNode = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
 
-        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
 
-        //노드 생성
-        for (int i = 0; i < N; i++){
-            graph.put(i+1, new ArrayList<>());
+        for (int i = 1; i <= N; i++)
+            graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int nodeA = Integer.parseInt(st.nextToken());
+            int nodeB = Integer.parseInt(st.nextToken());
+
+            graph[nodeA].add(nodeB);
+            graph[nodeB].add(nodeA);
         }
 
-        //노드 연결할 간선 입력(노드끼리 연결)
-        for (int i = 0; i < M; i++){
-            StringTokenizer stringTokenizer = new StringTokenizer(bf.readLine());
-            Integer nodeA = Integer.parseInt(stringTokenizer.nextToken());
-            Integer nodeB = Integer.parseInt(stringTokenizer.nextToken());
-
-            graph.get(nodeA).add(nodeB);
-            graph.get(nodeB).add(nodeA);
+        //노드 삽입 정보 오름차순으로 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
         }
 
-
-        for (int i = 0; i < N; i++){
-            Collections.sort(graph.get(i+1), Collections.reverseOrder());
-        }
-        dfsSearch(graph, startNode);
-
-        for (int i = 0; i < N; i++){
-            Collections.sort(graph.get(i+1));
-        }
-        bfsSearch(graph, startNode);
+        StringBuilder sb = new StringBuilder();
+        dfs(start, sb);
+        System.out.println(sb);
+        StringBuilder sb2 = new StringBuilder();
+        visited = new boolean[N + 1];
+        bfs(start, sb2);
+        System.out.println(sb2);
     }
 
-    //dfs는 스택을 이용.
-    public static void dfsSearch(HashMap<Integer, ArrayList<Integer>> graph, Integer start){
+    static void bfs(int start, StringBuilder sb) {
+        Queue<Integer> Q = new LinkedList<>();
 
-        ArrayList<Integer> stack = new ArrayList<>();
-        ArrayList<Integer> visited = new ArrayList<>();
-        String result = "";
+        visited[start] = true;
+        Q.offer(start);
+        sb.append(start + " ");
 
-        stack.add(start);
+        while(!Q.isEmpty()) {
+            Integer cur = Q.poll();
 
-        while(!stack.isEmpty()){
-            Integer removed = stack.remove(stack.size()-1);
-
-            if (!visited.contains(removed)) {
-                result += removed + " ";
-                visited.add(removed);
-                stack.addAll(graph.get(removed));
+            for (int next : graph[cur]) {
+                // 인접한 노드들 중에서 아직 방문하지 노드가 있는 경우 정답에 추가해주고 추가 탐색을 위해 큐에 해당 정점번호를 넣어준다.
+                if (!visited[next]) {
+                    sb.append(next + " ");
+                    visited[next] = true;
+                    Q.offer(next);
+                }
             }
         }
-        result = result.substring(0, result.length()-1);
-        System.out.print(result);
-        System.out.println();
     }
+    static void dfs(int cur, StringBuilder sb) {
+        // 체크인
+        visited[cur] = true;
+        sb.append(cur + " ");
 
-    //queue를 이용.
-    public static void bfsSearch(HashMap<Integer, ArrayList<Integer>> graph, Integer start){
-        ArrayList<Integer> queue = new ArrayList<>();
-        ArrayList<Integer> visited = new ArrayList<>();
-        String result = "";
-
-        queue.add(start);
-
-        while(!queue.isEmpty()){
-            Integer removed = queue.remove(0);
-
-            if (!visited.contains(removed)) {
-                visited.add(removed);
-                queue.addAll(graph.get(removed));
-                result += removed + " ";
+        for (int next : graph[cur]) {
+            if (!visited[next]) {
+                dfs(next, sb);
             }
         }
-        result = result.substring(0, result.length()-1);
-        System.out.print(result);
-        System.out.println();
     }
 }
