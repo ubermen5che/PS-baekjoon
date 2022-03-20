99b28e1fa0daa0c0c3112535567a4c84c90b9143
Yongjun Kwon <76172759+ubermen5che@users.noreply.github.com>
2022-03-20 23:43:09 +0900

    [Bronze III] Title: A+B - 3, Time: 124 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int T;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+            sb.append(sum).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
