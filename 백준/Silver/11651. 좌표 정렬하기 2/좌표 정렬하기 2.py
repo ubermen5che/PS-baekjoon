824bd74fd74a9db24e174e5bf053d9e127f984c7
Yongjun Kwon <76172759+ubermen5che@users.noreply.github.com>
2022-04-13 16:32:02 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 360 ms, Memory: 51616 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin
+n = int(input.readline())
+
+cord_list = []
+for i in range(n):
+    cord_list.append(tuple(map(int, input.readline().split())))
+
+cord_list.sort(key=lambda x: (x[1], x[0]))
+
+for i in cord_list:
+    print(i[0], i[1])
