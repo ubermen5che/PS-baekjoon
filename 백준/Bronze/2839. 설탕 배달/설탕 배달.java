10667ea0525c2fc2faadf08301b60c228b3358dd
Yongjun Kwon <76172759+ubermen5che@users.noreply.github.com>
2022-05-28 03:06:48 +0900

    [Bronze I] Title: 설탕 배달, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/2839. 설탕 배달/설탕 배달.java
+++ 백준/Bronze/2839. 설탕 배달/설탕 배달.java
@@ -2,41 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int N;
+    static int[] dy = new int[5001];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        int answer = -1;
-        int Q, R;
-        int weight = N;
-        // case1 : 5kg 봉지로 전부 담을 수 있을 경우 -> best
-        if (N % 5 == 0) {
-            answer = N / 5;
-            System.out.println(answer);
-            System.exit(0);
-        }
-        // case2 : 5Kg봉지와 3Kg봉지 두가지를 이용해서 설탕을 담는 경우 (5Kg봉지를 많이 사용하도록)
-        Q = weight / 5;
+        int amount = sc.nextInt();
 
-        // 5Kg 봉지를 하나씩 줄여가면서 최소 봉지 갯수를 구한다.
-        int Q2, R2;
-        for (int i = Q; i >= 1; i--) {
-            // 현재 설탕 무게 기준 3Kg봉지 몇개를 사용해야하는지 구한다.
-            int tmp = weight - (i * 5);
-            Q2 = tmp / 3;
-            R2 = tmp % 3;
-            if (R2 == 0) {
-                answer = i + Q2;
-                System.out.println(answer);
-                System.exit(0);
+        //init
+        dy[3] = 1;
+        dy[5] = 1;
+
+        for (int i = 6; i <= 5000; i++) {
+            if (dy[i-3] == 0 && dy[i-5] == 0) continue; // 5kg 3kg 둘다 이용해서 담을 수 없는 경우 ex) 4kg
+            if (dy[i-3] == 0 && dy[i-5] != 0) dy[i] = dy[i-5] + 1;
+            if (dy[i-3] != 0 && dy[i-5] == 0) dy[i] = dy[i-3] + 1;
+            if (dy[i-3] != 0 && dy[i-5] != 0) {
+                dy[i] = Math.min(dy[i-3], dy[i-5]) + 1;
             }
         }
 
-        // case 3 : 3Kg봉지만으로 설탕을 담는 경우
-        if (N % 3 == 0) {
-            answer = N / 3;
+        if (dy[amount] == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dy[amount]);
         }
-
-        System.out.println(answer);
     }
 }
